7bda3008f6358b667e1a7ba24d526fb2aa6cd9b7
sunkk6 <sunkk2333@gmail.com>
2022-03-07 23:06:17 +0800

    1 change

--- Fourier.py
+++ Fourier.py
@@ -3,22 +3,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def get_mse(true, pred):   # 定义损失函数（均方差）
+def get_mse(true, pred):  # 定义损失函数（均方差）
     if len(true) == len(pred):
         # return sum([(x - y) ** 2 for x, y in zip(true, pred)]) / len(true)
-        return sum([(np.array(true) - np.array(pred)) ** 2]) / len(true)
+        return np.sum([(np.array(true) - np.array(pred)) ** 2]) / len(true) * 100
     else:
         return None
 
 
+epoch = 3
+weight = np.ones(10)
+sin = {}
 f = 1 / 0.8
 t = np.linspace(0, 1, 500, endpoint=False)
 square = signal.square(2 * np.pi * f * t)
-sin1 = np.sin(2 * np.pi / 0.8 * t - 0)
-sin2 = np.sin(2 * 2 * np.pi / 0.8 * t - 0)
-sin3 = np.sin(3 * 2 * np.pi / 0.8 * t - 0)
+sin_all = 0
+for i in range(len(weight)-1):
+    sin[i] = weight[i] * np.sin((i+1) * 2 * np.pi / 0.8 * t)
+    sin_all = sin_all + sin[i]
+# for i in range(len(weight)):
+
+loss = get_mse(square, sin_all)
 plt.plot(t, square)
-plt.plot(t, sin1)
+plt.plot(t, sin_all)
 plt.ylim(-2, 2)
 plt.show()
-print(get_mse(square, sin1))
+# print(weight)
+print(loss)
+# for i in range(epoch):
+#     x = x - alpha * 2 * (x - 10)
+#     y = y - alpha * 2 * (y - 10)
+#     f = fxy(x, y)
+#     print("第{}次迭代：x=%f，y=%f，fxy=%f" % (i + 1, x, y, f))
